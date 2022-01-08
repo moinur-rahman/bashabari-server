db1424f2ad89768321fcb7ff12d13d33ea39d774
moinur-rahman <rahmanmoin27@gmail.com>
2022-01-08 11:39:37 +0600

    After completing filtering

--- src/routers/user.js
+++ src/routers/user.js
@@ -17,11 +17,58 @@ router.post("/userOwner", async (req, res) => {
 
 router.post("/userWorker", async (req, res) => {
   const user = new UserWorker(req.body);
-  console.log(user);
+
   try {
     await user.save();
     res.status(201).send(user);
   } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.post("/userWorker/search", async (req, res) => {
+  const { age, education, jobDescription, gender, address, salaryRange } =
+    req.body;
+
+  var filter = {};
+
+  if (education) {
+    filter = {
+      ...filter,
+      education,
+    };
+  }
+  if (jobDescription) {
+    filter = {
+      ...filter,
+      jobDescription,
+    };
+  }
+  if (gender) {
+    filter = {
+      ...filter,
+      gender,
+    };
+  }
+  if (address) {
+    filter = {
+      ...filter,
+      address,
+    };
+  }
+  if (salaryRange) {
+    filter = {
+      ...filter,
+      salaryRange,
+    };
+  }
+
+  console.log(filter);
+
+  try {
+    const user = await UserWorker.find(filter);
+    res.status(201).send(user);
+  } catch (error) {
     res.status(400).send(e);
   }
 });
